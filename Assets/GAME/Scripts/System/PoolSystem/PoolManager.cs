2dd8e6308234e6a183dacd07681d891932aa81af
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-03-18 11:37:53 +0100

    [Quentin] Sauvegarde, je retape le système de combat

--- Assets/GAME/Scripts/System/PoolSystem/PoolManager.cs
+++ Assets/GAME/Scripts/System/PoolSystem/PoolManager.cs
@@ -14,13 +14,7 @@ namespace CrabMaga
         [TableList(ShowPaging = true)]
         public List<Unit> unitsQueue = new List<Unit>();
 
-
         public Vector3 poolPosition = new Vector3(100, 0, 100);
-
-        private void Start()
-        {
-            
-        }
 
         public Crab PoolCrabToUnit(CrabUnit crabUnit)
         {
