0d7c22914672aef23ffdec10e53dfbabe2f50d50
QuentinROUSSEL <quent.roussel98@gmail.com>
2020-03-26 18:05:47 +0100

    [Quentin] Create Empty Game Object Tool

---
+++ Assets/QRTools/Utilities/Tools/CreateEmptyGameObjectTool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace QRTools
+{
+    public class CreateEmptyGameObjectTool : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [MenuItem("GameObject/Empty GameObjects With Graphics 3D", false, 0)]
+        static void CreateGameObjectWith3DGraphics()
+        {
+            GameObject go = new GameObject();
+            go.name = "Nom De Object";
+            go.transform.position = Vector3.zero;
+            Selection.activeObject = go;
+
+            GameObject gr = new GameObject();
+            gr.transform.position = Vector3.zero;
+            gr.transform.parent = go.transform;
+            gr.name = "Graphics";
+            gr.AddComponent<MeshFilter>();
+            gr.AddComponent<MeshRenderer>();
+        }
+#endif
+    }
+}
